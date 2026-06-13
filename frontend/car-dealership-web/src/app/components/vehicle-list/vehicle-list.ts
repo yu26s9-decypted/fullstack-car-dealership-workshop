c1823c18a468bcb02e6c85690d0a5572efc69ab3
andy <andyt.cyb@gmail.com>
2026-06-12 20:55:49 -0400

    implemented vehiclelist component with ngoninit

--- frontend/car-dealership-web/src/app/components/vehicle-list/vehicle-list.ts
+++ frontend/car-dealership-web/src/app/components/vehicle-list/vehicle-list.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { VehicleService } from '../../services/vehicle.service';
+import { Vehicle } from '../../models/vehicle.model';
 
 @Component({
   selector: 'app-vehicle-list',
@@ -6,4 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './vehicle-list.html',
   styleUrl: './vehicle-list.css',
 })
-export class VehicleList {}
+export class VehicleList implements OnInit{
+  private vehicleService = inject(VehicleService);
+
+  vehicles = signal<Vehicle[]>([])
+
+  ngOnInit(): void {
+    this.loadVehicle();
+  }
+
+  loadVehicle(){
+    this.vehicleService.getAllVehicles().subscribe({
+      next: (vehicles) => {
+        this.vehicles.set(vehicles);
+      },
+      error: (err) => {
+        console.log("Something went wrong.", err)
+      }
+    })
+  }
+}
