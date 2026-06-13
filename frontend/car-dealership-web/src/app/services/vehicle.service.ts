18a0877a4eb21b0b200de9e6708d95aea2713159
andy <andyt.cyb@gmail.com>
2026-06-12 20:48:31 -0400

    added vehicle service

--- frontend/car-dealership-web/src/app/services/vehicle.service.ts
+++ frontend/car-dealership-web/src/app/services/vehicle.service.ts
@@ -1,6 +1,17 @@
 import { Observable } from "rxjs";
 import { Vehicle } from "../models/vehicle.model";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environment/environment";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
 
 export class VehicleService{
-    
+    constructor(private http: HttpClient) {}
+
+    getAllVehicles(): Observable<Vehicle[]> {
+        return this.http.get<Vehicle[]>(`${environment.baseURL}/vehicle`)
+    }
 }
