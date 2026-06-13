638992ebe7a78011793dd5eb71d46d52a6842c64
andy <andyt.cyb@gmail.com>
2026-06-12 20:50:44 -0400

    added vehicle list component scaffold via the angular cli

---
+++ frontend/car-dealership-web/src/app/components/vehicle-list/vehicle-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-list',
+  imports: [],
+  templateUrl: './vehicle-list.html',
+  styleUrl: './vehicle-list.css',
+})
+export class VehicleList {}
