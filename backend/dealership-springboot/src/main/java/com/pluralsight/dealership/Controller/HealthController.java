162af0bb05a56c11bd7f40c22fc76ce41e781b34
andy <andyt.cyb@gmail.com>
2026-06-13 23:06:00 -0400

    added healthcontroller for monitoring via uptimerobot

---
+++ backend/dealership-springboot/src/main/java/com/pluralsight/dealership/Controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.pluralsight.dealership.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/status")
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("ok");
+    }
+}
